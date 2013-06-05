f30da8150a0a69652ce39945757f86a0ce6393d9
kenny vanpaemel <kennyvanpaemel@gmail.com>
2013-06-05 12:49:26 +0200

    restapi
    functions added

--- restapi.php
+++ restapi.php
@@ -25,6 +25,81 @@ $app->get('/users/:burger_id','getUsersForBurger');
 $app->get('/burgers/:taste_id','getBurgersByTaste');
 $app->get('/burgers/:vegi','getVegiBurgers');
 
+$app->post('/burgers','addBurger');
+$app->post('/users','addUser');
+$app->post('/comments/:burger_id/:user_id','addComment');
+
+$app->put('/burgers/:burger_id','updateBurger');
+$app->put('/burgers/:burger_id/:rating','updateBurgerRating');
+
 $app->run();
 
-?>
+function getAllBurgers()
+{
+	$burgersDAO = new BurgersDAO();
+}
+
+function getAllUsers()
+{
+	$usersDAO = new UsersDAO();
+}
+
+function getAllIngredients()
+{
+	$ingredientsDAO = new IngredientsDAO();
+}
+
+function getAllComments()
+{
+	$commentsDAO = new CommentsDAO();
+}
+
+function getAllTastes()
+{
+	$tastesDAO = new TastesDAO();
+}
+
+function getBurgerForUser($user_id)
+{
+	$burgersDAO = new BurgersDAO();
+}
+
+function getUsersForBurger($burger_id)
+{
+	$usersDAO = new UsersDAO();
+}
+
+function getBurgersByTaste($taste_id)
+{
+	$burgersDAO = new BurgersDAO();
+}
+
+function getVegiBurgers($vegi)
+{
+	$burgersDAO = new BurgersDAO();
+}
+
+function addBurger()
+{
+	$burgersDAO = new BurgersDAO();
+}
+
+function addUser()
+{
+	$usersDAO = new UsersDAO();
+}
+
+function addComment($burger_id,$user_id)
+{
+	$commentsDAO = new CommentsDAO();
+}
+
+function updateBurger($burger_id)
+{
+	$burgersDAO = new BurgersDAO();
+}
+
+function updateBurgerRating($burger_id,$rating)
+{
+	$burgersDAO = new BurgersDAO();
+}

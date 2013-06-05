ab5b9a0b93a367384d805eae49fad9deaabc306b
kenny vanpaemel <kennyvanpaemel@gmail.com>
2013-06-05 13:03:51 +0200

    restapi
    get functies aanvullen

--- restapi.php
+++ restapi.php
@@ -24,6 +24,7 @@ $app->get('/burgers/:user_id','getBurgerForUser');
 $app->get('/users/:burger_id','getUsersForBurger');
 $app->get('/burgers/:taste_id','getBurgersByTaste');
 $app->get('/burgers/:vegi','getVegiBurgers');
+$app->get('/ingredients/:ingredient_id','getIngredientById');
 
 $app->post('/burgers','addBurger');
 $app->post('/users','addUser');
@@ -37,46 +38,70 @@ $app->run();
 function getAllBurgers()
 {
 	$burgersDAO = new BurgersDAO();
+	echo json_encode($burgersDAO->getAllBurgers());
+	exit();
 }
 
 function getAllUsers()
 {
 	$usersDAO = new UsersDAO();
+	echo json_encode($usersDAO->getAllUsers());
+	exit();
 }
 
 function getAllIngredients()
 {
 	$ingredientsDAO = new IngredientsDAO();
+	echo json_encode($ingredientsDAO->getAllIngredients());
+}
+
+function getIngredientById($ingredient_id)
+{
+	$ingredientsDAO = new IngredientsDAO();
+	echo json_encode($ingredientsDAO->getIngredientById($ingredient_id));
+	exit();
 }
 
 function getAllComments()
 {
 	$commentsDAO = new CommentsDAO();
+	echo json_encode($commentsDAO->getAllComments());
+	exit();
 }
 
 function getAllTastes()
 {
 	$tastesDAO = new TastesDAO();
+	echo json_encode($tastesDAO->getAllTastes());
+	exit();
 }
 
 function getBurgerForUser($user_id)
 {
 	$burgersDAO = new BurgersDAO();
+	echo json_encode($burgersDAO->getBurgerForUser($user_id));
+	exit();
 }
 
 function getUsersForBurger($burger_id)
 {
 	$usersDAO = new UsersDAO();
+	echo json_encode($usersDAO->getUsersForBurger($burger_id));
+	exit();
 }
 
 function getBurgersByTaste($taste_id)
 {
 	$burgersDAO = new BurgersDAO();
+	echo json_encode($burgersDAO->getBurgersByTaste($taste_id));
+	exit();
 }
 
-function getVegiBurgers($vegi)
+function getVegiBurgers()
 {
 	$burgersDAO = new BurgersDAO();
+	echo json_encode($burgersDAO->getVegiBurgers());
+	exit();
 }
 
 function addBurger()

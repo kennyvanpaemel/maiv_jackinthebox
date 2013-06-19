9a5881384f16abc2cd7ac85d74f99cdd759132e8
kenny vanpaemel <kennyvanpaemel@gmail.com>
2013-06-19 20:19:22 +0200

    j
    g

--- classes/Config.php
+++ classes/Config.php
@@ -2,21 +2,10 @@
 
 class Config
 {
-    /*const DB_TYPE = 'mysql';
+    const DB_TYPE = 'mysql';
     const DB_HOST = 'localhost';
     const DB_USER = 'jack_usr';
     const DB_PASS = 'test123$';
-    const DB_NAME = 'jackinthebox2';*/
+    const DB_NAME = 'jackinthebox2';
 
-    const DB_TYPE = 'mysql';
-    const DB_HOST = 'mysqlstudent';
-    const DB_USER = 'bastiaanandriess';
-    const DB_PASS = 'ieKexepak4ee';
-    const DB_NAME = 'bastiaanandriess';
-
-    /*const DB_TYPE = 'mysql';
-    const DB_HOST = 'localhost';
-    const DB_USER = 'root';
-    const DB_PASS = 'root';
-    const DB_NAME = 'crosstheline';*/
 }
